6b8ed0357a986e34575857117eee411abbcf610b
Michal Bláha <michal@michalblaha.cz>
2019-02-12 03:19:35 +0100

    Update IncidentsResponse.cs

--- Cachet.NET/Responses/IncidentsResponse.cs
+++ Cachet.NET/Responses/IncidentsResponse.cs
@@ -6,73 +6,7 @@
 
     using RestSharp.Deserializers;
 
-    public partial class IncidentsResponse : GeneralResponse<List<IncidentObject>>
+    public partial class IncidentsResponse : GeneralCollectionResponse<IncidentObject>
     {
-            set;
-        }
-
-        /// <summary>
-        /// Gets or sets the Incidents.
-        /// </summary>
-        [DeserializeAs(Name = "data")]
-        public List<IncidentObject> Incidents
-        {
-            get;
-            set;
-        }
-
-        public class MetaObject
-        {
-            /// <summary>
-            /// Gets or sets the pagination.
-            /// </summary>
-            public PaginationObject Pagination
-            {
-                get;
-                set;
-            }
-
-            public class PaginationObject
-            {
-                public int Total
-                {
-                    get;
-                    set;
-                }
-
-                public int Count
-                {
-                    get;
-                    set;
-                }
-
-                public int PerPage
-                {
-                    get;
-                    set;
-                }
-
-                public int CurrentPage
-                {
-                    get;
-                    set;
-                }
-
-                public int TotalPages
-                {
-                    get;
-                    set;
-                }
-
-                public LinksObject Links
-                {
-                    get;
-                    set;
-                }
-
-                public class LinksObject
-                {
-                    public int NextPage
-                    {
     }
 }
