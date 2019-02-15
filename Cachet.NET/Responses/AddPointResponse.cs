103bde06785f1cd9b2682f7bff3b2e3f75cfd58d
Leroy.Haoyu.Liu <lhy20010403@hotmail.com>
2019-02-15 23:39:05 +0800

    Added metric point support

---
+++ Cachet.NET/Responses/AddPointResponse.cs
@@ -0,0 +1,9 @@
+namespace Cachet.NET.Responses
+{
+    using System.Collections.Generic;
+
+    using global::Cachet.NET.Responses.Objects;
+
+    using RestSharp.Deserializers;
+    public class AddPointResponse : GeneralSimpleResponse<PointObject> { }
+}
